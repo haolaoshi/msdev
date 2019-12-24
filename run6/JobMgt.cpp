02a3cc4b63f31e0390490ffeb2a2fa7f78c63e27
haolaoshi <wantong-liu@163.com>
2019-12-24 23:34:32 +0800

    Job

--- run6/JobMgt.cpp
+++ run6/JobMgt.cpp
@@ -1,8 +1,29 @@
 #include <Windows.h>
 #include <tchar.h>
 #include "JobMgt.h"
+#ifdef _UNICODE 
+#define _memtchr wmemchr
+#else
+#define _memtchr memchr
+#endif
+DWORD Options(int argc, LPTSTR argv[], LPCTSTR OptStr, ...)
+{
+	va_list pFlagList;
+	LPBOOL pFlag;
+	int iFlag = 0, iArg;
 
+	va_start(pFlagList, OptStr);
+	while ((pFlag = va_arg(pFlagList, LPBOOL)) != NULL && iFlag < (int)_tcslen(OptStr)) {
+		*pFlag = FALSE;
+		for (iArg = 1; !(*pFlag) && iArg < argc && argv[iArg][0] == _T('-'); iArg++)
+			*pFlag = _memtchr(argv[iArg], OptStr[iFlag], _tcslen(argv[iArg])) != NULL;
+		iFlag++;
+	}
+	va_end(pFlagList);
+	for (iArg = 1; iArg < argc && argv[iArg][0] == _T('-'); iArg++);
 
+	return iArg;
+}
 
 LONG GetJobNumber(PROCESS_INFORMATION* processInfo, LPCTSTR command)
 {
